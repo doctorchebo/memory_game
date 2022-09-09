fc6bdc54f5a0a57e55f2006cd289ee7517498a9e
Marcelo Paul Munoz Coaquira <MarceloPaul.Munoz@jalasoft.com>
2022-09-09 11:25:16 -0400

    persisting data in json file

---
+++ src/main/java/memory/GameConstants.java
@@ -0,0 +1,12 @@
+package memory;
+
+public class GameConstants {
+    private GameConstants() { }
+    public static final int NUMBER_OF_ROWS = 4;
+    public static final int NUMBER_OF_COLUMNS = 6;
+    public static final int NUMBER_OF_PAIRS = (NUMBER_OF_ROWS*NUMBER_OF_COLUMNS)/2;
+    public static final String URL_SUCCESS_ICON = "/src/main/java/images/done";
+    public static final String URL_UNKNOWN_ICON = "/src/main/java/images/unknown";
+    public static final String URL_NORMAL_ICON = "/src/main/java/images/";
+
+}
